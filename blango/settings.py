513619e5b1c80d9683c1f45e938826c35cd5023c
Javier Jiménez <javier.guillot@gmail.com>
2024-02-06 00:43:56 +0000

    Finish django registration.

--- blango/settings.py
+++ blango/settings.py
@@ -195,6 +195,10 @@ class Dev(Configuration):
 
     INTERNAL_IPS = ['192.168.10.226']
 
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+    ACCOUNT_ACTIVATION_DAYS = 7
+
 
 class Prod(Dev):
     DEBUG = False
